e04796251aa2e79299f3d447940ff09ee5d86f6d
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-15 21:49:11 +0000

    Adding multiple tags

--- app/app.rb
+++ app/app.rb
@@ -19,9 +19,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(title: params[:title], url: params[:url])
-    tag  = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    link = Link.create(title: params[:title], url: params[:url])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.first_or_create(name: tag)
+    end
     link.save
     redirect 'links'
   end
