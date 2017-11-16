fd050f049a57f68301a2788efb9adfb448368ba1
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-16 23:36:28 +0000

    Improved sign-up process

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,13 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
+
 # require_relative 'models/link'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
   get '/' do
@@ -36,14 +39,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-  user = User.create(email: params[:email],
-    password: params[:password], password_confirmation: params[:password_confirmation])
-  session[:user_id] = user.id
-  redirect to('/links')
+    @user = User.new(email: params[:email],
+      password: params[:password], password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
